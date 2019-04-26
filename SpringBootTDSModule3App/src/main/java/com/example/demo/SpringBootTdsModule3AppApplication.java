c8ad07bb53a4719e2219d9485db835c1db9a4a65
Kavithasanganamouni <48015974+Kavithasanganamouni@users.noreply.github.com>
2019-04-26 13:45:53 +0530

    Add files via upload

---
+++ SpringBootTDSModule3App/src/main/java/com/example/demo/SpringBootTdsModule3AppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTdsModule3AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTdsModule3AppApplication.class, args);
+	}
+
+}
